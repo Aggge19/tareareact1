fa16f0e9a183f3113c2493993b6b3f23ff2a354e
Aggge19 <84148803+Aggge19@users.noreply.github.com>
2021-12-07 15:58:09 +0100

    Create app.jsx

---
+++ src/app.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Home from './components/Home';
+
+const renderApplication = () => {
+    ReactDOM.render( <
+        Home / > ,
+        document.querySelector('#root')
+    );
+}
+
+renderApplication(Home);
+
+if (module.hot) {
+    module.hot.accept("./components/Home", () => {
+        renderApplication();
+    });
+}
